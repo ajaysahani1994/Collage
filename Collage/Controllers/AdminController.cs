c8a7731b0000af6bf58f4a1692188ff33e277d77
ajaysahani1994 <43149980+ajaysahani1994@users.noreply.github.com>
2018-09-11 00:35:25 +0530

    set up

---
+++ Collage/Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Collage.DataModel;
+namespace Collage.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        VasmmEntities db;
+        public AdminController()
+        {
+            db = new VasmmEntities();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult StafDetail()
+        {
+            var model = db.FACULITYDETAILs.ToList();
+            return View(model);
+        }
+        public ActionResult AddStafDetail()
+        {
+
+            return View();
+        }
+        public ActionResult LatestNews()
+        {
+
+            return View();
+        }
+
+    }
+}
